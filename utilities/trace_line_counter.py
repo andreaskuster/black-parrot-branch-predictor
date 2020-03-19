9de887d772e8b4005ce921c832afb2f329746767
kustera <kustera@student.ethz.ch>
2020-03-19 00:37:51 -0400

    Move trace line count utility and rename it.

--- utilities/trace_line_counter.py
+++ utilities/trace_line_counter.py
@@ -28,13 +28,13 @@ if __name__ == "__main__":
     """
     Counts the number of traces in all trace files.
     """
-    _BASE_PATH = "traces"
+    _BASE_PATH = ""
 
     traces = [f for f in os.listdir(_BASE_PATH) if os.path.isfile(os.path.join(_BASE_PATH, f)) and f.endswith(".trace")]
 
     for trace in traces:
         counter = 0
-        with open(os.path.join("traces", trace)) as file:
+        with open(os.path.join("", trace)) as file:
             for line in file:
                 counter += 1
         print("{}: {}".format(trace, counter))
