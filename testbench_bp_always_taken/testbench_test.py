16e546834c49f84bb93fc5dc1b0740def21a95d4
andreaskuster <mail@andreaskuster.ch>
2020-03-27 17:12:51 +0100

    Remove generic branch predictor RTL simulator functions. Adjust to refactored classes.

--- testbench_bp_always_taken/testbench_test.py
+++ testbench_bp_always_taken/testbench_test.py
@@ -24,97 +24,10 @@ __license__ = "GPL"
 
 import cocotb
 
-from cocotb.triggers import Timer, RisingEdge
-from cocotb.result import TestFailure
+from cocotb.triggers import RisingEdge
 
 from testbench_bp_always_taken import BranchPredictorAlwaysTaken
-from base import TraceReader
-
-clock_period = 1000
-
-
-@cocotb.coroutine
-def clock_gen(signal, period=10000):
-    while True:
-        signal <= 0
-        yield Timer(period / 2)
-        signal <= 1
-        yield Timer(period / 2)
-
-
-@cocotb.coroutine
-def bp_init(dut):
-    # reset
-    dut.reset_i = 1
-
-    # write branch result
-    dut.w_v_i = 0
-    dut.idx_w_i = 0
-    dut.correct_i = 0
-
-    # read branch prediction
-    dut.r_v_i = 0
-    dut.idx_r_i = 0
-    dut.predict_o = 0
-
-    # simulate a clock edge
-    yield RisingEdge(dut.clk_i)
-
-
-@cocotb.coroutine
-def bp_reset(dut):
-    # reset
-    dut.reset_i <= 1
-
-    # simulate a clock edge
-    yield RisingEdge(dut.clk_i)
-
-    # remove reset flag
-    dut.reset_i <= 0
-
-    # simulate a clock edge
-    yield RisingEdge(dut.clk_i)
-
-
-@cocotb.coroutine
-def bp_update(dut, index, correct):
-    # write
-    dut.w_v_i <= 1
-    dut.idx_w_i <= index
-    dut.correct_i <= correct
-
-    # simulate a clock edge
-    yield RisingEdge(dut.clk_i)
-
-    dut.w_v_i <= 0
-    dut.idx_w_i <= 0
-    dut.correct_i <= 0
-
-    # simulate a clock edge
-    yield RisingEdge(dut.clk_i)
-
-
-@cocotb.coroutine
-def bp_predict(dut, index, model_prediction):
-
-    # read
-    dut.r_v_i <= 1
-    dut.idx_r_i <= index
-
-    # simulate a clock edge
-    yield RisingEdge(dut.clk_i)
-
-    # read prediction
-    dut_prediction = int(dut.predict_o.value)
-    dut._log.info("Got value: {}".format(dut_prediction))
-    if dut_prediction != model_prediction:
-        raise TestFailure("Mismatch detected: dut {}, model {}".format(dut_prediction, model_prediction))
-
-    dut.r_v_i <= 0
-    dut.idx_r_i <= 0
-
-    # simulate a clock edge
-    yield RisingEdge(dut.clk_i)
+from base import TraceReader, BpRtlSim
 
 
 @cocotb.test()
@@ -122,13 +35,13 @@ def branch_predictor_basic(dut):
     """branch predictor basic"""
 
     # setup clock gen
-    cocotb.fork(clock_gen(dut.clk_i, period=clock_period))
+    cocotb.fork(BpRtlSim.clock_gen(dut.clk_i, period=1000))
 
     # init vars
-    yield bp_init(dut)
+    yield BpRtlSim.bp_init(dut)
 
     # reset
-    yield bp_reset(dut)
+    yield BpRtlSim.bp_reset(dut)
 
     # instantiate trace reader with the dummy trace
     tr = TraceReader("../traces/dummy.trace")
@@ -141,13 +54,13 @@ def branch_predictor_basic(dut):
         # predict branch
         pred_model = bp.predict(address)
         # wait for the simulation
-        yield bp_predict(dut, address, pred_model)
+        yield BpRtlSim.bp_predict(dut, address, pred_model)
         # check if prediction was correct
         correct = pred_model == taken
         # update internal predictor state
         bp.update(address, correct)
         # wait for the simulation
-        yield bp_update(dut, address, correct)
+        yield BpRtlSim.bp_update(dut, address, correct)
 
     yield RisingEdge(dut.clk_i)
     dut._log.info("Co-simulation finished.")
