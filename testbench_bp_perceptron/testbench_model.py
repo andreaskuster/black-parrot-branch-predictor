b67c557c87a3cb61794d3870569b4126f9c53249
andreaskuster <mail@andreaskuster.ch>
2020-03-27 23:37:04 +0100

    Refactor perceptron model.

--- testbench_bp_perceptron/testbench_model.py
+++ testbench_bp_perceptron/testbench_model.py
@@ -22,45 +22,17 @@ __author__ = "Andreas Kuster"
 __copyright__ = "Copyright 2020"
 __license__ = "GPL"
 
-import os
 import argparse
 
 import numpy as np
 
-
-class ShiftRegister:
-
-    def __init__(self, size, init_val=0):
-        self.size = size
-        self.register = [init_val]*size
-
-    def shift(self, new_item):
-        new_item = int(new_item)
-        if new_item not in [0, 1]:
-            print("Warning: not a binary number.")
-        self.register = [new_item] + self.register[0:self.size-1]
-
-    def reg_to_number(self):
-        number = 0
-        for i in range(self.size):
-            number += (2 ** i)*self.register[i]
-        return number
-
-
-class TraceReader:
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def read(self):
-        with open(self.filename, "r") as file:
-            for line in file:
-                address, taken = line.split()
-                yield int(address)//4, int(taken)  # remove lowest 2 bits from address, since they are always 0
+from base import Evaluator, TraceReader, ShiftRegister
 
 
 class BranchPredictorPerceptron:
     """
+        TODO: add description
+
         implementation details:
             - integer weights
             - input: boolean
@@ -68,6 +40,13 @@ class BranchPredictorPerceptron:
     """
 
     def __init__(self, bht_addr_bits, n_hist, learning_rate=1):
+        """
+        Initialize internal state.
+        :param bht_addr_bits: number of address bits for the branch history table index and the branch history shift register
+        :param n_hist: number of branch history bits
+        :param learning_rate: perceptron learning rate
+        :return: None
+        """
         # save params
         self.bht_addr_bits = bht_addr_bits
         self.n_hist = n_hist
@@ -80,6 +59,12 @@ class BranchPredictorPerceptron:
         self.gh = ShiftRegister(size=n_hist)
 
     def int_to_bit_vec(self, number, length=None):
+        """
+        Convert the integer value to its bit representation as a vector.
+        :param number: input integer value
+        :param length: length of bit vector
+        :return: corresponding bit vector
+        """
         bin_no = [int(x) for x in np.binary_repr(number)]
         if length is None:
             return bin_no
@@ -87,6 +72,12 @@ class BranchPredictorPerceptron:
             return [0]*(length-len(bin_no)) + bin_no
 
     def update(self, address, correct):
+        """
+        Update internal state using the information from the most recent prediction result.
+        :param address: branch address
+        :param correct: flag indicating whether the last prediction was correct
+        :return: None
+        """
         # prepare perceptron input
         inputs = ((address * (2 ** self.n_hist)) + self.gh.reg_to_number()) % (2**self.n_inputs)
         # convert int to bit vector
@@ -102,8 +93,12 @@ class BranchPredictorPerceptron:
         # update bias
         self.weights[0] += self.learning_rate * (label - prediction)
 
-
     def predict(self, address):
+        """
+        Predict branch using the internal state and the branch address.
+        :param address: branch address
+        :return: prediction
+        """
         # prepare perceptron input
         inputs = ((address * (2 ** self.n_hist)) + self.gh.reg_to_number()) % (2**self.n_inputs)
         # convert int to bit vector
@@ -114,64 +109,49 @@ class BranchPredictorPerceptron:
         return 20 if activation > 10 else 0
 
 
-def evaluate(sat_bits, addr_bits, n_hist, trace):
-
-    n_correct = 0
-    n_total = 0
-
-    tr = TraceReader(trace)
-    bp = BranchPredictorPerceptron(sat_cnt_bits=sat_bits, bht_addr_bits=addr_bits, n_hist=n_hist)
-
-    for address, taken in tr.read():
-        prediction = bp.predict(address)
-        correct = prediction == taken
-        bp.update(address, correct)
-        if correct:
-            n_correct += 1
-        n_total += 1
-    result = [trace, sat_bits, addr_bits, n_correct / n_total]
-    print(result)
-    return result
-
-
 if __name__ == "__main__":
 
+    # process command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--gridsearch", action="store_true")
     args = parser.parse_args()
 
+    # debugging
     if args.debug:
 
-        # params
+        # instantiate trace reader with the dummy trace
+        tr = TraceReader("../traces/dummy.trace")
+
+        # instantiate branch predictor
         _BHT_ADDR_BITS = 4
         _N_HIST = 0
-
-        tr = TraceReader("../evaluation/traces/dummy2.trace")
         bp = BranchPredictorPerceptron(bht_addr_bits=_BHT_ADDR_BITS, n_hist=_N_HIST)
 
+        # loop over all trace lines
         for address, taken in tr.read():
-            # remove upper bits
-            address = address % (2**_BHT_ADDR_BITS)
             # predict branch
-            prediction = 1 if bp.predict(address) > 10 else 0
-            # determine if prediction was correct
+            prediction = bp.predict(address)
+            # check if prediction was correct
             correct = prediction == taken
-            # update internal state
+            # update internal predictor state
             bp.update(address, correct)
 
+    # parameter grid search
     if args.gridsearch:
-        from multiprocessing import Pool
-        tasks = list()
-        with Pool(processes=64) as pool:
+
+        # instantiate evaluator
+        ev = Evaluator(branch_predictor=BranchPredictorPerceptron, num_processes=4)
+
+        # add all evaluation corners
+        for trace in ["short_mobile_1", "long_mobile_1", "short_server_1", "long_server_1"]:
             for sat_bits in [1, 2, 3, 4, 5]:
                 for addr_bits in [3, 6, 9, 12, 15]:
-                    for trace in ["short_mobile_1", "long_mobile_1", "short_server_1", "long_server_1"]:
-                        tasks.append(pool.apply_async(evaluate, (sat_bits, addr_bits, addr_bits, os.path.join("../evaluation/traces", trace + ".trace"))))
-            results = list()
-            for task in tasks:
-                results.append(task.get())
-            with open("gridsearch_perceptron.txt", "w") as file:
-                for result in results:
-                    file.write(str(result) + "\n")
-        print("done")
+                        ev.submit(trace, sat_bits, addr_bits)
+
+        # wait for all tasks to finish
+        ev.finalize()
+        # write result to file
+        ev.write_result("gridsearch_bp_gshare.csv")
+        # print result
+        ev.print_result()
