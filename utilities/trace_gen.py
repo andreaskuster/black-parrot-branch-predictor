c44249fd84fdbc8cc5dfcd0e04431366dea81739
andreaskuster <mail@andreaskuster.ch>
2020-03-31 11:12:51 +0200

    Refactor trace generator. Add code comments.

--- utilities/trace_gen.py
+++ utilities/trace_gen.py
@@ -23,6 +23,7 @@ __copyright__ = "Copyright 2020"
 __license__ = "GPL"
 
 import numpy as np
+
 from random import randint
 from enum import Enum
 
@@ -35,25 +36,25 @@ class TraceCmd(Enum):
 
 
 if __name__ == "__main__":
+    """
+    This program auto-generates traces for the gcd hardware accelerator developed as an assignment in the UW CSE 567 
+    (Principles Of Digital Systems Design).
+    """
     _TRACE_FILE = "trace.tr"
-    _WAIT_CMD = "0000"
-    _FINISH_CMD = "0100"
-    _SEND_CMD = "0001"
-    _RECV_CMD = "0010"
     _PADDING = "{0:032b}".format(0)
     with open(_TRACE_FILE, "w") as file:
         # wait K=3 cycles
         K = 3
         for i in range(K):
-            file.write("{}__{}__{}{}".format(_WAIT_CMD, _PADDING, _PADDING, "\n"))
+            file.write("{}__{}__{}{}".format(TraceCmd.WAIT, _PADDING, _PADDING, "\n"))
 
-        # generate L=2^12 examples
-        L = 2**3
+        # generate L=2^10 examples
+        L = 2**10
         L = min(L, 2 ** 31 - K - 1)
         for i in range(L):
             a, b = randint(0, (2 ** 32) - 1), randint(0, (2 ** 32) - 1)
-            file.write("{}__{}__{}{}".format(_SEND_CMD, "{0:032b}".format(a), "{0:032b}".format(b), "\n"))
-            file.write("{}__{}__{}{}".format(_RECV_CMD, "{0:032b}".format(0), "{0:032b}".format(np.gcd(a, b)), "\n"))
+            file.write("{}__{}__{}{}".format(TraceCmd.SEND, "{0:032b}".format(a), "{0:032b}".format(b), "\n"))
+            file.write("{}__{}__{}{}".format(TraceCmd.RECV, "{0:032b}".format(0), "{0:032b}".format(np.gcd(a, b)), "\n"))
 
         # finish sim
-        file.write("{}__{}__{}{}".format(_FINISH_CMD, _PADDING, _PADDING, "\n"))
+        file.write("{}__{}__{}{}".format(TraceCmd.FINISH, _PADDING, _PADDING, "\n"))
